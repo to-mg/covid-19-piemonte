1e44146dcdc61755cfb3c4e3ec9ffb0ce316597c
to-mg <mg-to@gmail.com>
2021-01-04 13:31:13 +0100

    gestione 2021

--- scarica_dati_da_regione_piemonte.py
+++ scarica_dati_da_regione_piemonte.py
@@ -23,11 +23,14 @@ import time
 import requests
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException, WebDriverException
-# import chrome_extensions
 
-UA = ("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
-      "Chrome/39.0.2171.95 Safari/537.36")
-REQUEST_HEADERS = {'User-Agent': UA}
+# import chrome_extensions
+ANNO_CORRENTE = "2021"
+UA = (
+    "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) "
+    "Chrome/39.0.2171.95 Safari/537.36"
+)
+REQUEST_HEADERS = {"User-Agent": UA}
 
 
 def load_comuni(datafolder):
@@ -35,21 +38,23 @@ def load_comuni(datafolder):
     load code and name for each comune from istat provided csv file
     create a dictionary mapping code to name
     """
-    url_istat = "https://www.istat.it/storage/codici-unita-amministrative/" + \
-                "Elenco-comuni-italiani.csv"
+    url_istat = (
+        "https://www.istat.it/storage/codici-unita-amministrative/"
+        + "Elenco-comuni-italiani.csv"
+    )
     ifile = Path(datafolder) / "Elenco-comuni-italiani.csv"
     if not os.path.isfile(ifile):
         print("Downloading ", ifile, " from ISTAT")
         buf = requests.get(url_istat, headers=REQUEST_HEADERS).text
-        open(ifile, 'wt', encoding='ISO-8859-1').write(buf)
+        open(ifile, "wt", encoding="ISO-8859-1").write(buf)
     comuni = {}
-    with open(ifile, 'rt', encoding='ISO-8859-1') as fin:
+    with open(ifile, "rt", encoding="ISO-8859-1") as fin:
         reader = csv.reader(fin, delimiter=";")
         for row in reader:
-            if row[0].find('Codice') >= 0:
+            if row[0].find("Codice") >= 0:
                 # skip header row
                 continue
-            elif row[0] != '01':
+            elif row[0] != "01":
                 # done with Piemonte
                 break
             else:
@@ -65,18 +70,18 @@ def get_istat_code_data(driver, comuni, rowlen, key):
     # javascript code for getting the data associated to each ISTAT code
     get_value_function = "return classificationData[arguments[0]];"
 
-    row = rowlen*[""]
+    row = rowlen * [""]
     item = driver.execute_script(get_value_function, key)
     try:
-        item['nome'] = comuni[item['comune_ist']]
+        item["nome"] = comuni[item["comune_ist"]]
     except KeyError:
-        item['nome'] = "Sconosciuto"
+        item["nome"] = "Sconosciuto"
         print("ISTAT code %s not found" % key)
-    print(item['nome'])
-    row[0] = item['nome']
-    row[1] = item['comune_ist']
+    print(item["nome"])
+    row[0] = item["nome"]
+    row[1] = item["comune_ist"]
     try:
-        row[2] = item['pop_tot']
+        row[2] = item["pop_tot"]
     except KeyError:
         row[2] = -1
     # write the data for each reported day
@@ -84,18 +89,18 @@ def get_istat_code_data(driver, comuni, rowlen, key):
     if "RAPPORTO" in item:
         data = zip(item["POSITIVI"], item["RAPPORTO"])
         for positives, ratio in data:
-            row[3] = positives['value']
-            row[4] = ratio['value']
-            month = positives['giorno'][:2]
-            day = positives['giorno'][2:]
-            row[5] = "2020/" + month + "/" + day
+            row[3] = positives["value"]
+            row[4] = ratio["value"]
+            month = positives["giorno"][:2]
+            day = positives["giorno"][2:]
+            row[5] = ANNO_CORRENTE + "/" + month + "/" + day
     else:
         for positives in item["POSITIVI"]:
-            row[3] = positives['value']
+            row[3] = positives["value"]
             row[4] = -1
-            month = positives['giorno'][:2]
-            day = positives['giorno'][2:]
-            row[5] = "2020/" + month + "/" + day
+            month = positives["giorno"][:2]
+            day = positives["giorno"][2:]
+            row[5] = ANNO_CORRENTE + "/" + month + "/" + day
     return row
 
 
@@ -109,8 +114,7 @@ def main():
     """
     datafolder = "data"
     comuni = load_comuni(datafolder)
-    url = "https://www.regione.piemonte.it/web/" + \
-          "covid-19-mappa-dei-contagi-piemonte"
+    url = "https://www.regione.piemonte.it/web/" + "covid-19-mappa-dei-contagi-piemonte"
 
     # create data folder
     Path(datafolder).mkdir(parents=True, exist_ok=True)
@@ -126,7 +130,7 @@ def main():
     # driver = chrome_extensions.start_chrome_flexible(options)
     driver.implicitly_wait(10)
 
-# navigate
+    # navigate
     try:
         driver.get(url)
     except TimeoutException:
@@ -134,14 +138,14 @@ def main():
         sys.exit()
     time.sleep(3)
 
-# move to map frame
-    iframe = driver.find_elements_by_tag_name('iframe')
+    # move to map frame
+    iframe = driver.find_elements_by_tag_name("iframe")
     if not iframe:
         print("Iframe not found, the page may have changed")
         sys.exit()
     driver.switch_to.frame(iframe[0])
 
-# get the keys (ISTAT codes) kept in javascript object classificationData
+    # get the keys (ISTAT codes) kept in javascript object classificationData
     keys = driver.execute_script("return Object.keys(classificationData)")
     if not keys:
         print("Javascript object classificationData not found in the frame")
@@ -155,11 +159,10 @@ def main():
     tmpfile = Path(datafolder) / ("dati_tmp_da_regione_piemonte.csv")
     # the most recent date found in the map is saved in last_date
     last_date = "2020/01/01"
-    with open(tmpfile, 'wt') as fout:
+    with open(tmpfile, "wt") as fout:
         writer = csv.writer(fout, delimiter=";")
         # write file header
-        row = ["Comune", "Codice ISTAT", "Abitanti",
-               "Positivi", "Rapporto", "Data"]
+        row = ["Comune", "Codice ISTAT", "Abitanti", "Positivi", "Rapporto", "Data"]
         writer.writerow(row)
 
         # loop on all the comuni via ISTAT code to get related data
@@ -171,8 +174,7 @@ def main():
 
     # last date is used to create output file name
     last_date = last_date.replace("/", "_")
-    ofile = Path(datafolder) / ("dati_" + last_date +
-                                "_da_regione_piemonte.csv")
+    ofile = Path(datafolder) / ("dati_" + last_date + "_da_regione_piemonte.csv")
     os.rename(tmpfile, ofile)
 
     driver.close()
